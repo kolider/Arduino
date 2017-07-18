5ba55b74599e29826dbf297cf95c3a48425765d7
kolider <kolider@i.ua>
2017-07-18 13:29:05 +0300

    Send request and getting response

--- projects/GSM_LiGu/GSM_LiGu.ino
+++ projects/GSM_LiGu/GSM_LiGu.ino
@@ -1,22 +1,31 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial SerialGSM (2,3);
+SoftwareSerial SSerial (2,4);
 
 boolean sendCommandGSM(char* command);
 boolean initialGSM (void);
-boolean sendRequest(char* request);
+String sendRequest(char* request);
+String Response();
+void clearSerial(void);
 
 void setup() {
     
+    SSerial.begin(9600);
+    
     Serial.begin(9600);
     while(!Serial);
-    
-    SerialGSM.begin(9600);
-    while(!SerialGSM);
+
+    do {
+        Serial.write("AT\r\n");
+        Serial.flush();
+        delay(20);
+    }while(!Serial.available());
+    while(Serial.available()){
+        Serial.read();
+        delay(20);        
+    }
 
     while(!initialGSM());
-
-    sendRequest("{\"s01\":\"o\",\"s02\":\"d\"}");
     
 }
 
@@ -25,28 +34,18 @@ void loop() {
 }
 
 boolean sendCommandGSM(char* command)
-{    
-    do {
-        SerialGSM.write("AT\r\n");
-        SerialGSM.flush();
-        delay(20);
-    }while(!SerialGSM.available());
-    while(SerialGSM.available()){
-        SerialGSM.read();
-        delay(20);        
-    }
-        
+{            
     String check = "";
     char control = "";
     int len = strlen(command);
     
     for (int i = 0; i < len; i++){
-        SerialGSM.write(command[i]);
-        SerialGSM.flush();
+        Serial.write(command[i]);
+        Serial.flush();
         delay(20);        
                                 //////////////////////////////////////////////////// Можливо тут вставити очікування для приходу відповіді від модуля
-        while(SerialGSM.available()){
-            control = (char)SerialGSM.read();
+        while(Serial.available()){
+            control = (char)Serial.read();
         }
         
         if (control != command[i])
@@ -57,123 +56,125 @@ boolean sendCommandGSM(char* command)
         control = "";
     }
 
-    SerialGSM.write("\r\n");
-    SerialGSM.flush();
+    Serial.write("\r\n");
+    Serial.flush();
     delay(20);    
     
-    while(SerialGSM.available()){
-        check += (char)SerialGSM.read();
+    while(Serial.available()){
+        check += (char)Serial.read();
         delay(20);
     }       
     if (check.indexOf("ERROR") >= 0){
-        Serial.print("Error: ");
-        Serial.println(command);
-        Serial.print(check);
+        SSerial.print("Error: ");
+        SSerial.println(command);
+        SSerial.print(check);
         return false;
     }
     else{
-        Serial.print(check);
+        SSerial.print("Success: ");
+        SSerial.println(command);
+        SSerial.print(check);
         return true;
     }    
 }
 
 boolean initialGSM(void)
-{
-    if (!sendCommandGSM("AT+CIPSHUT")) return false;
+{ 
+    while (!sendCommandGSM("AT+CIPSHUT"));
 
-    if (!sendCommandGSM("AT+CGDCONT=1,\"IP\",\"www.kyivstar.net\"")) return false;
+    while (!sendCommandGSM("AT+CGDCONT=1,\"IP\",\"www.kyivstar.net\""));
   
-    if (!sendCommandGSM("AT+CSTT=\"www.kyivstar.net\"")) return false;
+    while (!sendCommandGSM("AT+CSTT=\"www.kyivstar.net\""));
   
-    if (!sendCommandGSM("AT+CIICR")) return false;
+    while (!sendCommandGSM("AT+CIICR")){
+        delay(1000);
+        sendCommandGSM("AT+CIPSHUT");
+    }
 
-    if (!sendCommandGSM("AT+CIPSHUT")) return false;
+    while (!sendCommandGSM("AT+CIPSHUT"));
 
     return true;
 }
 
-boolean sendRequest(char* request)
+String sendRequest(char* request)
 {
     int len = strlen(request);
     
-    sendCommandGSM("AT+CIPSHUT");
-    delay(5000);
-  
-    SerialGSM.write("AT+CIFSR\r\n");
-    delay(2000);
-    
-    sendCommandGSM("AT+CIPSTART=\"TCP\",\"online.tyche.ua\",\"747\"");
+    while(!sendCommandGSM("AT+CIPSTART=\"TCP\",\"online.tyche.ua\",\"747\"")){
+        delay(1000);
+        sendCommandGSM("AT+CIPSHUT");
+    }
 
     delay(2000);
-  
-    //char data[] = "{\"s01\":\"c\",\"s02\":\"o\",\"s03\":\"d\"}\r\n";
     
-    SerialGSM.print("AT+CIPSEND\r\n");
+    Serial.print("AT+CIPSEND\r\n");
+    Serial.flush();
   
     delay(1000);
-    while(SerialGSM.available()){
-        Serial.print((char)SerialGSM.read());
-        delay(20);
-    }
     
-    SerialGSM.print("POST / HTTP/1.1\r\nHost: online.tyche.ua:747\r\n");
-    SerialGSM.flush();
+    Serial.print("POST /forTest/ HTTP/1.1\r\nHost: online.tyche.ua:747\r\n");
+    Serial.flush();
+    Serial.print("Connection: Keep-Alive\r\nContent-Type: application/json; charset=UTF-8\r\nContent-Length: ");
+    Serial.print(len+2);
+    Serial.print("\r\n\r\n");
 
-    while(SerialGSM.available()){
-        Serial.println("Point 1");
-        Serial.print((char)SerialGSM.read());
-        Serial.flush();
-        delay(20);
-        if (SerialGSM.available()) continue;
-        else delay(200);
-    }
-    
-    SerialGSM.print("Connection: Keep-Alive\r\nContent-Type: application/json; charset=UTF-8\r\nContent-Length: ");
-    SerialGSM.flush();
-    SerialGSM.print(len+2);
-    SerialGSM.flush();
-    SerialGSM.print("\r\n\r\n");
-    SerialGSM.flush();
-
-    while(SerialGSM.available()){
-        Serial.println("Point 2");
-        Serial.print((char)SerialGSM.read());
-        Serial.flush();
-        delay(20);
-        if (SerialGSM.available()) continue;
-        else delay(200);
-    }
+    Serial.flush();
   
-    SerialGSM.write(request);
-    SerialGSM.flush();
+    Serial.write(request);
+    Serial.write("\r\n");
+    Serial.flush();
     delay(500);
-    SerialGSM.write("\r\n");
-    SerialGSM.flush();
-    delay(20);
-    while(SerialGSM.available()){
-        Serial.println("Point 3");
-        Serial.print((char)SerialGSM.read());
-        Serial.flush();
-        delay(20);
-        if (SerialGSM.available()) continue;
+
+    clearSerial();
+    Serial.write((char)26);      //sendCommandGSM(""); 
+    while(!Serial.available());
+    
+    String responseVal = Response();
+        
+    while(!sendCommandGSM("AT+CIPSHUT")); 
+
+     return responseVal;
+
+}
+
+String Response()
+{
+    if ( Serial.available() ){
+        String response = "";
+        
+        while ( Serial.available() ){
+            char c = ""; 
+            Serial.readBytes(&c, 1);
+            response += c;
+            if ( Serial.available() ){
+                 continue;
+            }else {
+                delay(10);
+                if ( Serial.available() ){
+                    continue;
+                }
+                else delay (1000);
+            }    
+        }
+        if (response.length()){
+            uint8_t position = response.indexOf('{');
+            response.remove(0,position);
+            
+            position = response.length();
+            response = response.substring(0,position-2);
+        }
+        return response;
+        
+    }else{
+        return "";
+    }
+}
+
+void clearSerial(void)
+{
+     while(Serial.available()){
+        Serial.read();
+        if (Serial.available()) continue;
         else delay(200);
-    }
-  
-    delay(1000);
-    sendCommandGSM("");      //SerialGSM.write((char)26);   
-    while(SerialGSM.available()){
-        Serial.println("Point 4");
-        Serial.print((char)SerialGSM.read());
-        Serial.flush();
-        delay(20);
-    }
-  
-    delay(1000);
-    sendCommandGSM("AT+CIPSHUT");  
-    while(SerialGSM.available()){
-        Serial.println("Point 5");
-        Serial.print((char)SerialGSM.read());
-        Serial.flush();
-        delay(20);
     }
 }
